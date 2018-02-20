ea68bc3e494766b53577dd1c9e78556ec968d5a7
root <root@centos-512mb-sgp1-01.(none)>
2018-02-20 03:18:25 +0000

    Laravel5.5.* + Vuejs2.* + ElementUI2.* 環境構築

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Hello, {{ msg }}!</h1>
+        <router-link to="/hello">Hello</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'World'
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ resources/assets/js/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hello, Laravel 5.5.* + Vuejs2.* + ElementUI2.*</h1>
+        <el-button @click="visible = true">Button</el-button>
+        <el-dialog :visible.sync="visible">
+            <p>Message</p>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            visible: false
+        }
+    }
+}
+</script>
+
+<style>
+</style>
